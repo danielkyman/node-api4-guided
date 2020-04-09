698d243e543b1e57f8a5a98cf7a4557c17bce3dd
danielkyman <danny.kyman@gmail.com>
2020-04-09 10:28:12 -0700

    added test message to index url

--- api/server.js
+++ api/server.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const apiRouter = require('./api-router.js');
+const apiRouter = require("./api-router.js");
 
 const server = express();
 
 server.use(helmet());
 
-server.use('/api', apiRouter);
+router.get("/", (req, res) => {
+  res.status(200).json({ api: "HELLO" });
+});
+
+server.use("/api", apiRouter);
 
 module.exports = server;
